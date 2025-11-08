2d039bcfeb08238453a04661f8ee2e4746796362
DamoDevWorks <damian.wiewior98@gmail.com>
2025-11-08 12:42:35 +0100

    fix: prevent flash of unstyled content on initial load

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,5 +3,8 @@ import '../styles/main.scss';
 import { gsap } from 'gsap';
 import { bootLoader } from './utils/loader';
 
+// Mark styles ready (FOUC prevention)
+document.documentElement.classList.add('styles-ready');
+
 // ***** LOADER ****** // 
 bootLoader();
